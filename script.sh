9558120c8c1257d0039e1c66c27643ed6f4aa394
Tejas013 <tejasmadhukar6@gmail.com>
2023-10-25 02:18:41 +0530

    updated script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,19 @@
-
-
+#!/bin/bash
 
 if ! command -v youtube-comment-downloader &>/dev/null; then
     echo "Error: youtube-comment-downloader is not installed. Please install it before running this script."
     exit 1
 fi
 
-
 CSV_FILE="videos.csv"
-
 
 if [ ! -f "$CSV_FILE" ]; then
     echo "Error: CSV file $CSV_FILE not found."
     exit 1
 fi
 
-
 OUTPUT_DIR="downloaded_comments"
 mkdir -p "$OUTPUT_DIR"
-
 
 download_comments() {
     local youtube_id="$1"
@@ -28,9 +23,7 @@ download_comments() {
     echo "Downloaded comments for $youtube_id"
 }
 
-
-while IFS=',' read -r youtube_id _ || [ -n "$youtube_id" ]; do
-    
+while IFS=',' read -r youtube_id _ || [ -n "$youtube_id" ]; do    
     if [ "$youtube_id" != "youtube_id" ]; then
         download_comments "$youtube_id"
     fi
